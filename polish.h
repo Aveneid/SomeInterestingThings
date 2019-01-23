eea22fa065b2adf590191b6b633aa924a54ac1dd
Aveneid <dj.exers@gmail.com>
2019-01-23 23:43:10 +0100

    Add files via upload

---
+++ polish.h
@@ -0,0 +1,53 @@
+/**
+*	POLISH TRANSLATION PACK
+*	KEYWORDS FROM C++
+*	
+*	BY AVENEID 2019
+**/
+#define	dla	for
+#define	dopoki	while
+#define	rob	do
+#define	zwroc	return
+#define	jezeli	if
+#define	inaczej	else
+#define	przeloz	switch
+#define	klasa	class
+#define	struktura	struct
+#define	szablon	template
+#define	zabroniona	protected
+#define	publiczne	public
+#define	prywatne	private
+#define	dolacz	include
+#define	uzywaj	using
+#define	nowy	new
+#define	usun	delete
+#define	prawda	true
+#define	falsz	false
+#define	zlap	catch
+#define	probuj	try
+#define	rzuc	throw
+#define	to	this
+#define	lam	break
+#define	kontynuuj	continue
+#define	stala	const
+#define	domyslna	default
+#define	przypadek	case
+#define	wylicz	enum
+#define	przyjaciel	friend
+#define	rejestruj	register
+#define	calkowita	int
+#define	krotka	short
+#define	dluga	long
+#define	tekst	string
+#define	znak	char
+#define	przecinek	float
+#define	lepszyprzecinek	double
+#define	pustka	void
+#define	logiczna	bool
+#define	beznaku	usigned
+#define	statyczna	static
+#define	wirtualna	virtual
+#define	zeznakiem	signed
+#define wypluj cout
+#define	koniec endl
+#define wez cin
